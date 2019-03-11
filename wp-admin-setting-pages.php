b070d3bb3a57712dfe508c11a1b88bf9e6745076
Uğur Biçer <uuur86@yandex.com>
2019-03-11 20:09:36 +0300

    minor changes
    == Changelog
    * added single checkbox support
    * added segment support
    * added hidden fields support

--- wp-admin-setting-pages.php
+++ wp-admin-setting-pages.php
@@ -4,49 +4,126 @@
  * WP Settings framework
  *
  * @author Uğur Biçer <uuur86@yandex.com>
- * @copyright 2018, Uğur Biçer
+ * @copyright 2018 - 2019, Uğur Biçer
  * @license GPLv3
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
+ *
+ * https://github.com/uuur86/wpsettings
  */
+
 
 class wp_admin_setting_pages {
 
+    // String form title
     protected $title;
 
+    // String form description
     protected $desc;
 
+    // String wp language domain name
     protected $domain;
 
+    // String current page name
     protected $page_name;
 
+    // String current settings option name
     protected $settings_name;
 
+    // String current settings row
+    protected $settings_row;
+
+    // String current index name
+    protected $index_name;
+
+    // Array all indexes
+    protected $indexes;
+
+    // Array All input fields
     protected $fields;
 
+    // Array sanitize info
     protected $sanitize;
 
+    // Array all sections index
     protected $sections;
 
+    // String section name
     protected $section;
 
+    // String segment name
+    protected $segment;
 
 
-    public function __construct( $page, $settings, $domain ) {
+
+    public function __construct( $page, $setting_name, $domain, $row = false ) {
 
         if( !isset( $page ) || $page === false || empty( $page ) )
             return false;
 
+        $this->index_name = $setting_name . '_' . 'index';
+
+        if( $row !== false ) {
+
+            if( ( $this->indexes = $this->get_indexes() ) !== false && is_array( $this->indexes ) ) {
+                $last_index = array_flip( $this->indexes );
+                $last_index = end( $last_index );
+                $last_settings = get_option( $this->indexes[ $last_index ] );
+
+                if( $last_settings !== false && $row === true ) {
+                    $row = intval( $last_index );
+
+                    if( is_array( $last_settings ) && count( $last_settings ) > 0 )
+                        $row++;
+                }
+            }
+
+            $this->settings_row = $row;
+            $setting_name .= '_' . $this->settings_row;
+
+            if( false === get_option( $this->index_name ) ) {
+                add_option( $this->index_name, [ $this->settings_row => $setting_name . '_settings' ] );
+            }
+            else {
+                $this->indexes[ $this->settings_row ] = $setting_name . '_settings';
+                update_option( $this->index_name, $this->indexes );
+            }
+        }
+
         $this->page_name = $page;
-        $this->settings_name = $settings . '_settings'; // TODO : will be change soon
+        $this->settings_name = $setting_name . '_settings'; // TODO : will be change soon
         $this->domain = $domain;
 
-        if( false == get_option( $this->settings_name ) ) {
+        if( false === get_option( $this->settings_name ) ) {
             add_option( $this->settings_name, [] );
         }
 
         return $this;
     }
+
+
+
+    public function set_segment( $name ) {
+        if( !empty( $name ) )
+            $this->segment = $name;
+    }
+
+
+
+    public function if_segment( $name ) {
+        if( isset( $_POST[ 'segment_name' ] ) ) {
+            if( $name == $_POST[ 'segment_name' ] )
+                return true;
+        }
+
+        if( isset( $this->segment ) ) {
+            if( $name == $this->segment )
+                return true;
+        }
+
+        return false;
+    }
+
 
 
     public function add_section( $name, $title, $desc ) {
@@ -62,10 +139,10 @@ class wp_admin_setting_pages {
         ];
 
         add_settings_section(
-            $this->section . '_section', // Section Title
+            $this->section . '_section', // Section ID
             '',//esc_html__( $title, $this->domain ), // Callback for an optional title
             array( $this, 'settings_field_section_callback' ), // Admin page to add section to
-            $this->section
+            $this->section . ''
         );
     }
 
@@ -93,17 +170,22 @@ class wp_admin_setting_pages {
     public function add_new_field( $type, $id, $label, $options = null, $sanitize = 'text_field' ) {
         $field_type_callback = 'settings_field_' . $type . '_callback';
 
-        if( in_array( $type, array( 'radio', 'select' ) ) )
-            $this->sanitize[ $id ] = array( 'type' => 'options', 'values' => array_keys( $options ) );
-        else
+        if( in_array( $type, array( 'radio', 'select' ) ) ) {
+
+            if( is_array( $options ) ) {
+                $this->sanitize[ $id ] = array( 'type' => 'options', 'values' => array_keys( $options ) );
+            }
+        }
+        else {
             $this->sanitize[ $id ] = array( 'type' => $sanitize, 'regex' => $options );
+        }
 
         $field_args = [
             'label' => esc_html__( $label, $this->domain ),
-            'name' => $id
+            'name' => esc_html( $id )
         ];
 
-        if( is_array( $options ) ) {
+        if( !empty( $options ) ) {
             $field_args[ 'options' ] = $options;
         }
 
@@ -119,13 +201,31 @@ class wp_admin_setting_pages {
 
 
 
-    public function register() {
+    public function add_hidden_field( $id, $value ) {
+        $field_args = [
+            'value' => esc_html( $value ),
+            'name' => esc_html( $id )
+        ];
 
-        register_setting(
-            $this->settings_name . '',
-            $this->settings_name . '',
-            array( $this, 'settings_input_middleware' )
+        add_settings_field(
+            $this->settings_name . '_' . $id,
+            '',
+            array( $this, 'settings_field_hidden_input_callback' ),
+            $this->section,
+            $this->section . '_section',
+            $field_args
         );
+    }
+
+
+
+    public function settings_field_hidden_input_callback( $args ) {
+        $value = $args[ 'value' ];
+
+        if( !empty( $value ) ) {
+            $value = esc_html( $value );
+        }
+        echo '<input type="hidden" id="' . $this->settings_name . '_' . $args[ 'name' ] . '_input_text" name="' . $this->settings_name . '[' . $args[ 'name' ] . ']" value="' . $value . '"/>';
     }
 
 
@@ -183,6 +283,19 @@ class wp_admin_setting_pages {
                 $options .= $opt_val . '</label> &nbsp;<br/>';
             }
         }
+        else {
+            $value_ = '';
+            if( !empty( $value ) ) {
+                $value_ = esc_html( $value );
+            }
+
+            $id = $this->settings_name . '_' . $args[ 'name' ] . '_input_checkbox';
+            $name = $this->settings_name . '[' . $args[ 'name' ] . ']';
+
+            $options .= '<label for="' . $id . '">';
+            $options .= '<input type="checkbox"  name="' . $name . '" id="' . $id . '" value="' . $args[ 'options' ] . '" ' . checked( $args[ 'options' ], $value_, false ) . '/>';
+            $options .= '</label> &nbsp;<br/>';
+        }
 
         $options .= '</fieldset>';
 
@@ -209,7 +322,7 @@ class wp_admin_setting_pages {
                 $options .= $opt_val . '</label> &nbsp;<br/>';
             }
         }
-        
+
         $options .= '</fieldset>';
 
         echo $options;
@@ -251,18 +364,55 @@ class wp_admin_setting_pages {
 
 
 
+    public function register() {
+        global $_POST;
+
+        if( ( $all_settings = $this->get_indexes() ) !== false && isset( $_POST[ 'setting_name' ] ) ) {
+            $requested_option_page = $_POST[ 'setting_name' ];
+            if( in_array( $requested_option_page, $all_settings ) )
+                register_setting( $this->page_name, $requested_option_page, array( $this, 'settings_input_middleware' ) );
+        }
+        else {
+            register_setting( $this->page_name, $this->settings_name, array( $this, 'settings_input_middleware' ) );
+        }
+    }
+
+
+
+    public function put_hidden_fields() {
+        echo '<input type="hidden" name="segment_name" value="' . $this->segment . '"/>';
+        echo '<input type="hidden" name="setting_name" value="' . $this->settings_name . '"/>';
+    }
+
+
+
     public function run() {
         // Display necessary hidden fields for settings
-        settings_fields( $this->settings_name );
+        settings_fields( $this->page_name );
         // Display the settings sections for the page
         foreach( $this->sections as $sec_name => $sec_value ) {
             do_settings_sections( $sec_name );
         }
 
+        $this->put_hidden_fields();
         // Default Submit Button
         submit_button();
     }
 
-}
 
+
+    public function get_indexes() {
+        return get_option( $this->index_name );
+    }
+
+
+
+    public function last_index() {
+        if( $this->settings_row > -1 )
+            return $this->settings_row;
+
+        return false;
+    }
+
+}
 
